6c6e030e32b91c302723c3ccbd43310aee199a41
PawleN-D <sirpawle@gmail.com>
2023-10-05 20:09:14 +0200

    add correct routing pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
-import { AppProps } from 'next/app';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Header';
+// pages/LoginPage.tsx
+import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const LoginPage: React.FC = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1">
-        <Navbar />
-        {/* <Component {...pageProps} /> */}
+    <div dir='rtl' className="min-h-screen flex">
+      {/* Sidebar */}
+      <div className="w-1/2 bg-blue-500 rounded-s-lg p-8">
+        {/* Sidebar Content */}
+        <h1 className="text-4xl text-white mb-4">EMS</h1>
+        <p className="text-white">Beyond and Away</p>
+      </div>
+
+      
+      <div className="w-1/2 p-8">
+        
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        
+        <form>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700">Username</label>
+            <input type="text" id="username" name="username" className="border rounded-lg px-3 py-2 w-full" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700">Password</label>
+            <input type="password" id="password" name="password" className="border rounded-lg px-3 py-2 w-full" />
+          </div>
+          <button type="submit" className="bg-blue-500 text-white rounded-lg px-4 py-2">Login</button>
+        </form>
       </div>
     </div>
   );
-}
+};
 
-export default MyApp;
+export default LoginPage;
