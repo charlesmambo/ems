a7135b2d79ec36e6e232cce68e50c384bd5f2ae2
PawleN-D <sirpawle@gmail.com>
2023-10-06 01:05:51 +0200

    adjust add new code

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Header'
+import Sidebar from '../components/Sidebar'
+
+function Home() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
+        <Navbar />
+        {/* <Component {...pageProps} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Home
